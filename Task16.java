f139168697da54e32c3814db6c9f779adf4f390c
RyzhNyash <60988190+RyzhNyash@users.noreply.github.com>
2020-02-25 17:49:08 +0300

    Add files via upload

---
+++ Task16.java
@@ -0,0 +1,25 @@
+package by.ld.unit02.main;
+
+public class Task16 {
+	public static void main (String[] args) {
+		
+//	Вычислить: (1+2)*(1+2+3)*...*(1+2+...+10).	
+		
+		long a = 3;
+		
+		long b = a;
+		
+		int i = 0;
+		
+		for (i = 3; i <=10; i++ ) {
+		a += i;
+		b *= a;
+		
+		System.out.println(i + "|  " + a + "|  " + b);
+		}
+		
+		
+		
+	}
+
+}
