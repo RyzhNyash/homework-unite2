14a94347d8e2e990639b933eef6546568837b742
RyzhNyash <60988190+RyzhNyash@users.noreply.github.com>
2020-02-25 15:44:27 +0300

    Add files via upload

---
+++ Task19.java
@@ -0,0 +1,17 @@
+package by.ld.unit02.main;
+
+public class Task19 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		char j = (char) i;
+			
+		for (i = 0; i <= 127; i++) 
+		{
+			j = (char) i;
+			System.out.print(" |числовое значение>| " + i);
+			System.out.print(" |ASCII>| " + j);
+			System.out.println();
+		}
+	}
+}
