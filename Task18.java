98b4bb7340f8ad3f9e6295e76ccd3f83b8a0810f
RyzhNyash <60988190+RyzhNyash@users.noreply.github.com>
2020-03-02 12:18:45 +0300

    Update Task18.java

--- Task18.java
+++ Task18.java
@@ -3,52 +3,51 @@ package by.ld.unit02.main;
 import java.util.Scanner;
 
 public class Task18 {
-	public static void main (String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		double x = 0;
-		int a = 0;
-		int b = 0;
-		int h = 0;
-		double f = 0;
-		
-		System.out.print("число a > ");
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double x = 0;
+        int a = 0;
+        int b = 0;
+        int h = 0;
+        double f = 0;
+
+        System.out.print("число a > ");
         while (sc.hasNextInt() == false) {
             sc.nextLine();
             System.out.print("a > ");
         }
         a = sc.nextInt();
-		
+
         System.out.print("число b> ");
         while (sc.hasNextInt() == false) {
             sc.nextLine();
             System.out.print("b > ");
         }
         b = sc.nextInt();
-        
+
         System.out.print("число h> ");
         while (sc.hasNextInt() == false) {
             sc.nextLine();
             System.out.print("h > ");
         }
         h = sc.nextInt();
-        
-        
+
+
         x = a;
-		
-		while (x <= b) {
-			f = Math.sin(x) * Math.sin(x);
-			
-			f = f * 1000;
-			int f1 = (int)f;
-			f = (double)f1 / 1000;
-			x = x + h;
-			
-			System.out.println("значение x " + (int)x+ "|  |значение а " + f);
-			
-		}
-		
-	}
+
+        System.out.println("х" + "   |" + "f");
+
+        for (int i = a; i <= b; i++) {
+            x = x + h;
+            f = Math.pow(Math.sin(x), 2);
+            f = f * 1000;
+            int f1 = (int) f;
+            f = (double) f1 / 1000;
+            System.out.println((int)x + "   |" + f);
+        }
+
+    }
 
 }
